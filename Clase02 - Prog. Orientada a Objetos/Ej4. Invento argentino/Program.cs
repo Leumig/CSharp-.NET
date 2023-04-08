fa743c7cb90e67669acec0bf9a209f347ea28c40
Leumig <junmigue7@gmail.com>
2023-04-08 17:49:07 -0300

    Ejercicio 4 POO

---
+++ Clase02 - Prog. Orientada a Objetos/Ej4. Invento argentino/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using Librería_de_Clases;
+
+namespace Ej4._Invento_argentino
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Boligrafo boligrafoUno = new Boligrafo(100, ConsoleColor.Blue);
+            Boligrafo boligrafoDos = new Boligrafo(50, ConsoleColor.Red);
+
+            Console.WriteLine($"|| Cantidades de tinta || Azul:{boligrafoUno.Tinta}  Rojo: {boligrafoDos.Tinta}");
+            Console.WriteLine("===================================================== \n");
+
+            boligrafoUno.Recargar(); //Los recargo a su punto máximo (variable const)
+            boligrafoDos.Recargar(); //Los recargo a su punto máximo (variable const)
+            Console.WriteLine($"|| Cantidades de tinta || Azul: {boligrafoUno.Tinta}  Rojo: {boligrafoDos.Tinta}");
+            Console.WriteLine("===================================================== \n");
+
+            boligrafoUno.SetTinta(0); //Le agrego 0 de tinta (no pasará nada)
+            boligrafoDos.SetTinta(-10); //Le quito 10 de tinta
+            Console.WriteLine($"|| Cantidades de tinta || Azul: {boligrafoUno.Tinta}  Rojo: {boligrafoDos.Tinta}");
+            Console.WriteLine("===================================================== \n");
+
+            boligrafoUno.SetTinta(-200); //Le quito 200 de tinta (va a quedar en 0)
+            boligrafoDos.SetTinta(-90); //Le quito 90 de tinta (va a quedar en 0)
+            Console.WriteLine($"|| Cantidades de tinta || Azul: {boligrafoUno.Tinta}  Rojo: {boligrafoDos.Tinta}");
+            Console.WriteLine("===================================================== \n");
+
+            boligrafoUno.SetTinta(30); //Le agrego 30
+            boligrafoDos.SetTinta(20); //Le agrego 20
+            Console.WriteLine($"|| Cantidades de tinta || Azul: {boligrafoUno.Tinta}  Rojo: {boligrafoDos.Tinta}");
+            Console.WriteLine("===================================================== \n");
+
+            string dibujo;
+
+            if(boligrafoUno.Pintar(50, out dibujo)) // Pinto sólo 30 unidades (y queda en 0)
+            {                                       // Si pongo 0 o menos, no entra al if
+                Console.ForegroundColor = boligrafoUno.Color;
+                Console.WriteLine(dibujo);
+                Console.ResetColor();
+            }
+
+            if (boligrafoDos.Pintar(10, out dibujo))
+            {
+                Console.ForegroundColor = boligrafoDos.Color;
+                Console.WriteLine(dibujo);
+                Console.ResetColor();
+            }
+
+            Console.WriteLine($"|| Cantidades de tinta || Azul: {boligrafoUno.Tinta}  Rojo: {boligrafoDos.Tinta}");
+            Console.WriteLine("===================================================== \n");
+
+            if (boligrafoUno.Pintar(10, out dibujo)) 
+            {                                       
+                Console.ForegroundColor = boligrafoUno.Color;
+                Console.WriteLine(dibujo);
+                Console.ResetColor();
+            }
+
+            if (boligrafoDos.Pintar(10, out dibujo))
+            {
+                Console.ForegroundColor = boligrafoDos.Color;
+                Console.WriteLine(dibujo);
+                Console.ResetColor();
+            }
+
+            Console.WriteLine($"|| Cantidades de tinta || Azul: {boligrafoUno.Tinta}  Rojo: {boligrafoDos.Tinta}");
+            Console.WriteLine("===================================================== \n");
+
+            boligrafoUno.SetTinta(30); //Le agrego 30
+            boligrafoDos.SetTinta(20); //Le agrego 20
+            Console.WriteLine($"|| Cantidades de tinta || Azul: {boligrafoUno.Tinta}  Rojo: {boligrafoDos.Tinta}");
+            Console.WriteLine("===================================================== \n");
+
+            if (boligrafoDos.Pintar(80, out dibujo))
+            {
+                Console.ForegroundColor = boligrafoDos.Color;
+                Console.WriteLine(dibujo);
+                Console.ResetColor();
+            }
+
+            Console.WriteLine($"|| Cantidades de tinta || Azul: {boligrafoUno.Tinta}  Rojo: {boligrafoDos.Tinta}");
+            Console.WriteLine("===================================================== \n");
+        }
+    }
+}
---
+++ Librería de Clases/Boligrafo.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Librería_de_Clases
+{
+    public class Boligrafo
+    {
+        private const short cantidadTintaMaxima = 100;
+        private ConsoleColor color;
+        private short tinta;
+
+        public Boligrafo(short tinta, ConsoleColor color)
+        {
+            this.color = color;
+            this.tinta = tinta;
+        }
+
+        public ConsoleColor Color 
+        { 
+            get { return color; } 
+        }
+
+        public short Tinta
+        {
+            get { return tinta; }
+        }
+
+        public void SetTinta(short cantidadTinta)
+        {
+            //Console.WriteLine($"NOS LLEGO {cantidadTinta}");
+
+            short tintaResultante = this.tinta;
+            tintaResultante += cantidadTinta;
+
+            if(ValidarCantidadTinta(tintaResultante))
+            {
+                this.tinta = tintaResultante;
+            }
+            else
+            {
+                this.tinta = 0;
+            }
+        }
+
+        private bool ValidarCantidadTinta(short tinta)
+        {
+            return tinta >= 0 && tinta <= cantidadTintaMaxima;
+        }
+
+        public void Recargar()
+        {
+            this.tinta = cantidadTintaMaxima;
+        }
+
+        public bool Pintar(short gasto, out string dibujo)
+        {
+            if(gasto <= 0)
+            {
+                dibujo = "";
+                return false;
+            }else
+            {
+                short tintaAUtilizar = this.tinta;
+
+                if (gasto < this.tinta)
+                {
+                    tintaAUtilizar = gasto;
+                }
+
+                //Console.WriteLine($"EL GASTO AHORA MISMO ES {gasto}");
+                SetTinta( (short) - gasto );
+                //Console.WriteLine($"QUEDO DE TINTA: {Tinta}");
+
+                StringBuilder sb = new StringBuilder();
+
+                sb.AppendLine($"\n[Unidades: {tintaAUtilizar}]");
+                while (tintaAUtilizar > 0)
+                {
+                    sb.Append("" + "*");
+                    tintaAUtilizar--;
+                }
+
+                dibujo = sb.ToString();
+                return true;
+            }
+        }
+    }
+}
