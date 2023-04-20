deeb88393b7e3a72fe85fa8e8f23807c9a0661d4
Leumig <junmigue7@gmail.com>
2023-04-20 00:04:22 -0300

    Ejercicios

---
+++ Clase05 - Colecciones/I03. A contar palabras/Vista.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace I03._A_contar_palabras
+{
+    partial class Vista
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.rtb_Mensaje = new System.Windows.Forms.RichTextBox();
+            this.btn_Calcular = new System.Windows.Forms.Button();
+            this.btn_Enunciar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // rtb_Mensaje
+            // 
+            this.rtb_Mensaje.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.rtb_Mensaje.Location = new System.Drawing.Point(12, 12);
+            this.rtb_Mensaje.Name = "rtb_Mensaje";
+            this.rtb_Mensaje.Size = new System.Drawing.Size(333, 269);
+            this.rtb_Mensaje.TabIndex = 0;
+            this.rtb_Mensaje.Text = "";
+            // 
+            // btn_Calcular
+            // 
+            this.btn_Calcular.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btn_Calcular.Location = new System.Drawing.Point(241, 306);
+            this.btn_Calcular.Name = "btn_Calcular";
+            this.btn_Calcular.Size = new System.Drawing.Size(104, 37);
+            this.btn_Calcular.TabIndex = 1;
+            this.btn_Calcular.Text = "Calcular";
+            this.btn_Calcular.UseVisualStyleBackColor = true;
+            this.btn_Calcular.Click += new System.EventHandler(this.btn_Calcular_Click);
+            // 
+            // btn_Enunciar
+            // 
+            this.btn_Enunciar.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btn_Enunciar.Location = new System.Drawing.Point(12, 306);
+            this.btn_Enunciar.Name = "btn_Enunciar";
+            this.btn_Enunciar.Size = new System.Drawing.Size(127, 37);
+            this.btn_Enunciar.TabIndex = 2;
+            this.btn_Enunciar.Text = "Enunciar palabras";
+            this.btn_Enunciar.UseVisualStyleBackColor = true;
+            this.btn_Enunciar.Click += new System.EventHandler(this.btn_Enunciar_Click);
+            // 
+            // Vista
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(363, 355);
+            this.Controls.Add(this.btn_Enunciar);
+            this.Controls.Add(this.btn_Calcular);
+            this.Controls.Add(this.rtb_Mensaje);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Vista";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Contador de palabras";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private RichTextBox rtb_Mensaje;
+        private Button btn_Calcular;
+        private Button btn_Enunciar;
+    }
+}
---
+++ Clase05 - Colecciones/I03. A contar palabras/Vista.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace I03._A_contar_palabras
+{
+    /*Consigna
+    Crear un proyecto de tipo Windows Forms App, con un RichTextBox y un botón Calcular.*/
+    public partial class Vista : Form
+    {
+        public Vista()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_Calcular_Click(object sender, EventArgs e)
+        {
+            Dictionary<string, int> diccionario = new Dictionary<string, int>();
+
+            char[] separaciones = new char[] {' ', '\n', '-'};
+            //Creo un array con los caracteres que representan un espacio entre dos cadenas
+
+            string[] arrayPalabrasSeparadas = rtb_Mensaje.Text.Split(separaciones, StringSplitOptions.RemoveEmptyEntries);
+            //Creo un array de strings y le doy el valor del texto ingresado, el cual va a ser
+            //un array de strings dividido por las separaciones del array de separaciones
+            //'Split' recorre el string dividiendolo según las separaciones que le pasemos, y devuelve un array
+
+            foreach (var itemArray in arrayPalabrasSeparadas)
+            {
+                if(diccionario.ContainsKey(itemArray))
+                {
+                    diccionario[itemArray]++;
+                    //Esto funciona porque 'itemArray es un string', entonces vamos al elemento del
+                    //diccionario que tenga ese valor como Key. Y le hacemos ++ (modificando su Value).
+                }
+                else
+                {
+                    diccionario.Add(itemArray, 1);
+                }
+            }
+            
+            var diccionarioOrdenado = diccionario.OrderByDescending(x => x.Value);
+            //Creo un nuevo diccionario ordenado descendentemente según el Value
+
+            StringBuilder sb = new StringBuilder();
+            int indice = 1;
+
+            foreach (var itemDicciconario in diccionarioOrdenado.Take(3))
+            {
+                sb.AppendLine($"{indice}. {itemDicciconario.Key} [{itemDicciconario.Value}]");
+                indice++;
+            }
+
+            MessageBox.Show($"TOP 3 PALABRAS MÁS REPETIDAS\n{sb}", "Resultado");
+        }
+
+        private void btn_Enunciar_Click(object sender, EventArgs e)
+        {
+            char[] separaciones = new char[] { ' ', '\n', '-', ',' };
+            string[] arrayPalabrasSeparadas = rtb_Mensaje.Text.Split(separaciones, StringSplitOptions.RemoveEmptyEntries);
+
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine("LISTA DE PALABRAS\n");
+            
+            foreach (var itemPalabra in arrayPalabrasSeparadas)
+            {
+                sb.AppendLine($"- {itemPalabra}");
+            }
+
+            MessageBox.Show(sb.ToString(), "Enunciado");
+        }
+    }
+}
